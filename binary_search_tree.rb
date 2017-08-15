54db5af751fb00376a95b13f136f601ab171517a
Rathi <github email address>
2017-08-15 13:16:10 -0400

    included insert function

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,10 +1,27 @@
+
 class BST
   attr_accessor :data, :left, :right
 
-  def initalize(data)
+  def initialize(data)
     self.data = data
   end
 
-  def insert
-  end 
+  def insert(data)
+    if self.data >= data
+      if left.nil?
+        self.left = BST.new(data)
+      else
+        self.left.insert(data)
+      end
+    else
+      if right.nil?
+        self.right = BST.new(data)
+      else
+        self.right.insert(data)
+      end
+    end
+  end
+  
+  def each
+  end
 end
