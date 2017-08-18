22210f3b59c10adc54486db4fd27e518ad7cb635
Rathi <github email address>
2017-08-18 11:31:58 -0400

    included &block concept"

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -21,7 +21,10 @@ class BST
       end
     end
   end
-  
-  def each
-  end
+
+  def each(&block)
+    left.each(&block) if left
+      block.call(data)
+      right.each(&block) if right
+end
 end
